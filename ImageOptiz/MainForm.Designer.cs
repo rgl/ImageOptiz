c134025d9adaa1f6cc173b6922ddc8dfa643ea00
ruilopes.com <ruilopes.com@localhost>
2010-03-07 21:24:04 +0000

    Initial prototype.

---
+++ ImageOptiz/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ImageOptiz
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.listView = new System.Windows.Forms.ListView();
+            this.statusColumnHeader = new System.Windows.Forms.ColumnHeader();
+            this.fileColumnHeader = new System.Windows.Forms.ColumnHeader();
+            this.originalSizeColumnHeader = new System.Windows.Forms.ColumnHeader();
+            this.optimizedSizeColumnHeader = new System.Windows.Forms.ColumnHeader();
+            this.differenceSizeColumnHeader = new System.Windows.Forms.ColumnHeader();
+            this.helpLabel = new System.Windows.Forms.Label();
+            this.percentageOfOriginalColumnHeader = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.statusColumnHeader,
+            this.fileColumnHeader,
+            this.originalSizeColumnHeader,
+            this.optimizedSizeColumnHeader,
+            this.differenceSizeColumnHeader,
+            this.percentageOfOriginalColumnHeader});
+            this.listView.FullRowSelect = true;
+            this.listView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(0, -3);
+            this.listView.Name = "listView";
+            this.listView.ShowItemToolTips = true;
+            this.listView.Size = new System.Drawing.Size(564, 143);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // statusColumnHeader
+            // 
+            this.statusColumnHeader.Text = "";
+            this.statusColumnHeader.Width = 20;
+            // 
+            // fileColumnHeader
+            // 
+            this.fileColumnHeader.Text = "File";
+            this.fileColumnHeader.Width = 200;
+            // 
+            // originalSizeColumnHeader
+            // 
+            this.originalSizeColumnHeader.Text = "Original size";
+            this.originalSizeColumnHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.originalSizeColumnHeader.Width = 80;
+            // 
+            // optimizedSizeColumnHeader
+            // 
+            this.optimizedSizeColumnHeader.Text = "Optimized size";
+            this.optimizedSizeColumnHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.optimizedSizeColumnHeader.Width = 80;
+            // 
+            // differenceSizeColumnHeader
+            // 
+            this.differenceSizeColumnHeader.Text = "Difference";
+            this.differenceSizeColumnHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.differenceSizeColumnHeader.Width = 80;
+            // 
+            // helpLabel
+            // 
+            this.helpLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.helpLabel.AutoSize = true;
+            this.helpLabel.Location = new System.Drawing.Point(12, 159);
+            this.helpLabel.Name = "helpLabel";
+            this.helpLabel.Size = new System.Drawing.Size(262, 13);
+            this.helpLabel.TabIndex = 2;
+            this.helpLabel.Text = "To optimize images, drag&&drop them into this window";
+            // 
+            // percentageOfOriginalColumnHeader
+            // 
+            this.percentageOfOriginalColumnHeader.Text = "% Original";
+            this.percentageOfOriginalColumnHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.percentageOfOriginalColumnHeader.Width = 80;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 189);
+            this.Controls.Add(this.helpLabel);
+            this.Controls.Add(this.listView);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(520, 200);
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "ImageOptiz";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader statusColumnHeader;
+        private System.Windows.Forms.ColumnHeader fileColumnHeader;
+        private System.Windows.Forms.ColumnHeader originalSizeColumnHeader;
+        private System.Windows.Forms.ColumnHeader optimizedSizeColumnHeader;
+        private System.Windows.Forms.Label helpLabel;
+        private System.Windows.Forms.ColumnHeader differenceSizeColumnHeader;
+        private System.Windows.Forms.ColumnHeader percentageOfOriginalColumnHeader;
+    }
+}
+
---
+++ ImageOptiz/MainForm.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageOptiz
+{
+    public partial class MainForm : Form
+    {
+        private static readonly string[] ValidExtensions = new[]
+        {
+            ".png",
+            ".jpg",
+            ".jpeg",
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region AboutForm handling
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        private static extern bool InsertMenu(IntPtr hMenu, int wPosition, int wFlags, int wIDNewItem, string lpNewItem);
+
+        [DllImport("user32.dll")]
+        private static extern int GetMenuItemCount(IntPtr hMenu);
+
+        public const int WM_SYSCOMMAND = 0x112;
+        public const int MF_SEPARATOR = 0x800;
+        public const int MF_BYPOSITION = 0x400;
+        public const int MF_STRING = 0x0;
+        public const int IDM_ABOUT = 1000;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            var systemMenu = GetSystemMenu(Handle, false);
+            var baseMenuItemPosition = GetMenuItemCount(systemMenu) - 2;
+
+            InsertMenu(systemMenu, baseMenuItemPosition + 0, MF_BYPOSITION | MF_SEPARATOR, 0, string.Empty);
+            InsertMenu(systemMenu, baseMenuItemPosition + 1, MF_BYPOSITION | MF_STRING, IDM_ABOUT, "About");
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_SYSCOMMAND)
+            {
+                switch (m.WParam.ToInt32())
+                {
+                    case IDM_ABOUT:
+                        ShowAboutForm();
+                        return;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void ShowAboutForm()
+        {
+            using (var aboutForm = new AboutForm())
+            {
+                aboutForm.ShowDialog(this);
+            }
+        }
+
+        #endregion
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            var data = e.Data;
+
+            if (data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+
+            if (data.GetDataPresent(DataFormats.Text, true))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+
+#if DEBUG
+            Trace.WriteLine("MainForm_DragEnter Native formats:");
+            var nativeFormats = data.GetFormats(false);
+            foreach (var format in nativeFormats)
+                Trace.WriteLine(format);
+
+            Trace.WriteLine("MainForm_DragEnter Converted formats:");
+            var convertedFormats = data.GetFormats(true);
+            foreach (var format in convertedFormats)
+                Trace.WriteLine(format);
+#endif
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files == null)
+                {
+                    var text = (string)e.Data.GetData(DataFormats.Text);
+                    if (string.IsNullOrEmpty(text))
+                        return;
+                    files = text.Split('\n');
+                }
+
+                if (files.Length == 0)
+                    return;
+
+                // bring this form to the top.
+                Activate();
+
+                listView.Items.Clear();
+
+                QueueFiles(files);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(string.Format("Error in DragDrop method: {0}", ex.Message));
+                // NB: Do not show a MessageBox here; explorer is waiting.
+            }
+        }
+
+        private void QueueFiles(string[] fileNames)
+        {
+            foreach (var fileName in fileNames)
+                QueueFile(fileName);
+        }
+
+        private void QueueFile(string fileName)
+        {
+            Trace.WriteLine(string.Format("QueueFile {0}", fileName));
+
+            // TODO do this in background... using the manager.
+
+            if (Directory.Exists(fileName))
+            {
+                // TODO recurse the directory looking for images, and add them all.
+            }
+            else
+            {
+                // TODO queue the file...
+                var fileInfo = new FileInfo(fileName);
+
+                if (!ValidExtensions.Contains(fileInfo.Extension.ToLower()))
+                    return;
+
+                // TODO filter the file by extension.
+
+                var originalFileLength = fileInfo.Length;
+
+                var listViewItem = new ListViewItem(string.Empty);
+                listViewItem.SubItems.Add(fileInfo.Name);
+                listViewItem.SubItems.Add(string.Format("{0}", originalFileLength)); // TODO format the number
+
+                listView.Items.Add(listViewItem);
+
+                OptimizeImage(fileInfo);
+
+                fileInfo.Refresh();
+                var optimizedFileLength = fileInfo.Length;
+                listViewItem.SubItems.Add(string.Format("{0}", optimizedFileLength)); // TODO format the number
+                listViewItem.SubItems.Add(string.Format("{0}", optimizedFileLength - originalFileLength)); // TODO format the number
+                listViewItem.SubItems.Add(string.Format("{0:#.##}", (optimizedFileLength / (double)originalFileLength) * 100)); // TODO format the number
+            }
+        }
+
+        private void OptimizeImage(FileInfo fileInfo)
+        {
+            // TODO catch exceptions, and flag the image path has failed (with the error as tooltip).
+            switch (fileInfo.Extension.ToLower())
+            {
+                case ".png":
+                    RunPngOut(fileInfo);
+                    break;
+                case ".jpg":
+                case ".jpeg":
+                    RunJpegTran(fileInfo);
+                    break;
+            }
+        }
+
+        private ToolResult RunPngOut(FileInfo fileInfo)
+        {
+            // NB: do not run pngout with the -q argument (we want to output to the user).
+            return RunTool("pngout", "-y", fileInfo.FullName);
+        }
+
+        private ToolResult RunJpegTran(FileInfo fileInfo)
+        {
+            var temporaryFileInfo = CreateTemporaryFile(fileInfo);
+            var toolResult = RunTool("jpegtran", "-verbose", "-copy", "none", "-optimize", fileInfo.FullName, temporaryFileInfo.FullName);
+            temporaryFileInfo.Refresh();
+            if (toolResult.ExitCode == 0 && temporaryFileInfo.Exists && temporaryFileInfo.Length > 0 && fileInfo.Length > temporaryFileInfo.Length)
+            {
+                File.Delete(fileInfo.FullName);
+                File.Move(temporaryFileInfo.FullName, fileInfo.FullName);
+            }
+            else
+            {
+                File.Delete(temporaryFileInfo.FullName);
+            }
+            return toolResult;
+        }
+
+        private static FileInfo CreateTemporaryFile(FileInfo fileInfo)
+        {
+            FileInfo temporaryFileInfo;
+            do
+            {
+                var guid = Guid.NewGuid();
+                var temporaryFilePath = Path.Combine(fileInfo.DirectoryName, string.Format("{0}.tmp{1}", guid, fileInfo.Extension));
+                temporaryFileInfo = new FileInfo(temporaryFilePath);
+            }
+            while (temporaryFileInfo.Exists);
+            return temporaryFileInfo;
+        }
+
+        private class ToolResult
+        {
+            public int ExitCode { get; set; }
+            public string StandardOutput { get; set; }
+            public string StandardError { get; set; }
+        }
+
+        private ToolResult RunTool(string tool, params string[] arguments)
+        {
+            var toolPath = Path.Combine(Path.Combine(Application.StartupPath, "tools"), string.Format("{0}.exe", tool));
+            using (
+                var p = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = EscapeProcessArgument(toolPath),
+                        Arguments = EscapeProcessArguments(arguments),
+                        RedirectStandardInput = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                }
+            )
+            {
+                var standardOutput = new StringBuilder();
+                var standardError = new StringBuilder();
+
+                p.OutputDataReceived += (sendingProcess, e) => standardOutput.AppendLine(e.Data);
+                p.ErrorDataReceived += (sendingProcess, e) => standardError.AppendLine(e.Data);
+
+                if (!p.Start())
+                {
+                    throw new ApplicationException("Ops, failed to launch pngout");
+                }
+
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+
+                p.WaitForExit();
+
+                return new ToolResult {
+                    ExitCode = p.ExitCode,
+                    StandardOutput = standardOutput.ToString(),
+                    StandardError = standardError.ToString()
+                };
+            }
+        }
+
+        private static string EscapeProcessArguments(string[] arguments)
+        {
+            var sb = new StringBuilder();
+            foreach (var argument in arguments)
+            {
+                if (sb.Length > 0)
+                    sb.Append(' ');
+                EscapeProcessArgument(argument, sb);
+            }
+            return sb.ToString();
+        }
+
+        private static string EscapeProcessArgument(string argument)
+        {
+            var sb = new StringBuilder();
+            EscapeProcessArgument(argument, sb);
+            return sb.ToString();
+        }
+
+        private static void EscapeProcessArgument(string argument, StringBuilder sb)
+        {
+            // Normally, an Windows application (.NET applications too) parses
+            // their command line using the CommandLineToArgvW function. Which has
+            // some peculiar rules.
+            // See http://msdn.microsoft.com/en-us/library/bb776391(VS.85).aspx
+
+            // TODO how about backslashes? there seems to be a weird interaction
+            //      between backslahses and double quotes...
+            // TODO do test cases of this! even launch a second process that
+            //      only dumps its arguments.
+
+            if (argument.Contains('"'))
+            {
+                sb.Append('"');
+                // escape single double quotes with another double quote.
+                sb.Append(argument.Replace("\"", "\"\""));
+                sb.Append('"');
+            }
+            else if (argument.Contains(' ')) // AND it does NOT contain double quotes! (those were catched in the previous test)
+            {
+                sb.Append('"');
+                sb.Append(argument);
+                sb.Append('"');
+            }
+            else
+            {
+                sb.Append(argument);
+            }
+
+            // TODO what about null/empty arguments?
+        }
+    }
+}
